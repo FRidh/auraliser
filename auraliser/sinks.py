996adf9585ff4144df30a695341f1c2468acda60
Frederik Rietdijk <fridh@fridh.nl>
2015-02-06 14:25:51 +0100

    Sinks / Receivers

---
+++ auraliser/sinks.py
@@ -0,0 +1,93 @@
+"""Sinks.
+"""
+import abc
+import itertools
+from acoustics.ambisonics import acn
+from acoustics.directivity import SphericalHarmonic
+
+
+def encode(contributions, directivities, resolution=1000):
+    """Encode contributions with directivities.
+    
+    :param contributions: Contributions is an iterable containing tuples of two elements, 
+    with the first element the signal and the second element the Cartesian orientation.
+    :param directivities: Iterable containing directivities. Each item results in a signal to be outputted.
+    
+    """
+    contributions = list(contributions)
+    
+    for directivity in directivities:
+        yield sum( ( (signal * np.repeat(channel.directivity.using_cartesian(*orientation[::resolution].T), resolution)[0:len(signal)]) for signal, orientation in contributions) ) 
+
+def mono(contributions):
+    """Output to mono.
+    
+    :param contributions: Contributions
+    :returns: Signal
+    
+    """
+    return sum((signal for signal, orientation in contributions))
+    
+    
+def ambisonics(contributions, order, resolution=1000):
+    """Encode as ambisonics using ACN format.
+    """
+    directivities = (SphericalHarmonic(m=m, n=n) for n, m in acn)
+    yield from encode(contributions, directivities, order)
+ 
+ 
+
+
+
+#class _Encoder(object):
+    #"""Abstract encoder."""
+        
+    #@abc.abstractmethod
+    #def encode(self, contributions):
+        #"""Encode contributions with."""
+        #pass
+    
+#class Mono(Encoder):
+    #"""Mono encoder."""
+   
+    #def __init__(self):
+        #super().__init__(directivities=[Mono()])
+        
+
+
+       
+   
+    #def encode(self, contributions):
+        #return sum((signal for signal, contribution in contributions))
+    
+#class Stereo(Encoder):
+    #pass
+
+
+#class Encoder(object):
+    
+    #def __init__(self, directivities):
+        
+        #self._directivities = directivities
+
+    #def encode(self, contributions):
+        #yield from encode(contributions, directivities, resolution)
+
+#class Ambisonics(Encoder):
+    #"""Ambisonics encoder."""
+
+    #def __init__(self, order):
+        
+        #self.order = order
+        #"""Order."""
+        
+    #def acn(self):
+        #"""ACN."""
+        #yield from acn(self.order)
+        
+    #def encode(self, contributions):
+        
+        
+    
+    
+    
