252bb5fc958df51d57aa6fbafd78f2e2741fe3c4
Frederik Rietdijk <fridh@fridh.nl>
2017-05-11 16:49:01 +0200

    Add nix expression

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ buildPythonPackage
+, pytest
+, cython
+, acoustics
+, cytoolz
+, ism
+, numpy
+, scipy
+, matplotlib
+, numba
+, scintillations
+, turbulence
+}:
+
+buildPythonPackage rec {
+  name = "auraliser-${version}";
+  version = "0.0";
+
+  src = ./.;
+
+  preBuild = ''
+    make clean
+  '';
+
+  buildInputs = [ pytest cython ];
+  propagatedBuildInputs = [ acoustics cytoolz ism numpy
+    scipy matplotlib numba scintillations turbulence
+  ];
+
+  meta = {
+    description = "Auraliser";
+  };
+  doCheck = false;
+}
+
